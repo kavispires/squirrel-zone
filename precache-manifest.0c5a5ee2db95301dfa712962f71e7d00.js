380f59587a301822e567b6f77968ad462a926e16
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-09-07 16:05:39 -0700

    Updates

---
+++ precache-manifest.0c5a5ee2db95301dfa712962f71e7d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5a83a2b9f209b178ef4a2f41ca143e",
+    "url": "/squirrel-zone/index.html"
+  },
+  {
+    "revision": "8615a707ec04be14badd",
+    "url": "/squirrel-zone/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "72deb927e53b4d8bee30",
+    "url": "/squirrel-zone/static/css/main.5fb087dd.chunk.css"
+  },
+  {
+    "revision": "8615a707ec04be14badd",
+    "url": "/squirrel-zone/static/js/2.2de7ecd4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/squirrel-zone/static/js/2.2de7ecd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72deb927e53b4d8bee30",
+    "url": "/squirrel-zone/static/js/main.8d93dd2e.chunk.js"
+  },
+  {
+    "revision": "88143d403343b4d6f0d8",
+    "url": "/squirrel-zone/static/js/runtime-main.8e216fff.js"
+  }
+]);

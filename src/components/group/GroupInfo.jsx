95422bc54ef44bcc360c429213f86a8ab2ee35c1
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2021-02-19 13:18:38 -0800

    Fixed Years Active count

--- src/components/group/GroupInfo.jsx
+++ src/components/group/GroupInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function GroupInfo({ group }) {
   return (
     <div>
-      <p>Years Active: {group.disbandmentYear - group.debutYear}</p>
+      <p>Years Active: {group.disbandmentYear - group.debutYear + 1}</p>
       <p>Debut Year: {group.debutYear}</p>
       <p>Disbandment Year: {group.disbandmentYear}</p>
       <p>Number of Members: {group.membersIds.length}</p>

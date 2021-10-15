c3664363f88c7865c22499f5e3adfe4794532d4a
kavispires <kavispires@gmail.com>
2021-10-15 15:47:41 -0700

    Updated lyrics scroller to go to playhead current time

--- src/components/distribution/ViewLyricsScroller.jsx
+++ src/components/distribution/ViewLyricsScroller.jsx
@@ -1,31 +1,52 @@
 import React, { memo, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
+// State
+import useDistributorState from '../../states/useDistributorState';
 // Utils
 import { bemClass, getBemModifier } from '../../utils';
-
 // Components
 import ViewLyricEntry from './ViewLyricEntry';
+
+const getClosesTime = (currentTime, lyrics) => {
+  let closestIndex = 0;
+  for (let i = 0; i < lyrics.length; i++) {
+    if (lyrics[i].startTime > currentTime * 100) {
+      break;
+    }
+    closestIndex = i;
+  }
+
+  return closestIndex;
+};
 
 function ViewLyricsScroller({ currentTime, lyrics, dimensions }) {
   const lyricsRef = useRef([]);
   const [latestIndex, setLatestIndex] = useState(0);
-  // const [latestAcceptableTime, setLatestAcceptableTime] = useState(0);
+  const [isPlaying] = useDistributorState('isPlaying');
 
   // Center song title from the start
   useEffect(() => {
     lyricsRef.current[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
   }, []);
 
+  // In case of play head change or video paused and played
+  useEffect(() => {
+    if (isPlaying) {
+      if (currentTime * 1000 < lyrics[1].startTime) {
+        lyricsRef.current[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+        setLatestIndex(0);
+      } else {
+        const closestIndex = getClosesTime(currentTime, lyrics);
+        setLatestIndex(closestIndex);
+        lyricsRef.current[closestIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    }
+  }, [isPlaying]); // eslint-disable-line
+
   // If the next line is gonna start within 500ms, center it
   useEffect(() => {
     if (latestIndex === lyrics.length - 1) return;
-
-    // if (currentTime > 0 && currentTime < 0.3) {
-    //   lyricsRef.current[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
-    //   setLatestIndex(-1);
-    //   return;
-    // }
 
     if (lyrics[latestIndex + 1].startTime <= Math.trunc(currentTime * 1000) + 100) {
       lyricsRef.current[latestIndex + 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
